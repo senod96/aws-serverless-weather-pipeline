4a60c7396966627d3d56a311f5fe722288504bfa
Senod <senodmesandu123456@gmail.com>
2026-06-12 16:25:15 +0530

    Manage EventBridge schedule with Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,6 +11,39 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_iam_role" "scheduler_invoke_lambda_role" {
+  name = "weather-pipeline-scheduler-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "scheduler.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "scheduler_invoke_lambda_policy" {
+  name = "weather-pipeline-scheduler-invoke-lambda"
+  role = aws_iam_role.scheduler_invoke_lambda_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = "lambda:InvokeFunction"
+        Resource = "arn:aws:lambda:us-east-1:603013471165:function:weather-pdf-downloader"
+      }
+    ]
+  })
+}
+
 resource "aws_s3_bucket" "weather_bucket" {
   bucket = "weather-pipeline-yourname"
 }
@@ -66,4 +99,21 @@ resource "aws_cloudwatch_metric_alarm" "lambda_errors" {
   alarm_actions = [
     aws_sns_topic.weather_alerts.arn
   ]
+}
+
+resource "aws_scheduler_schedule" "daily_weather_download" {
+  name        = "daily-weather-pdf-download"
+  description = "Runs Lambda daily to download weather PDF to S3"
+
+  flexible_time_window {
+    mode = "OFF"
+  }
+
+  schedule_expression          = "cron(30 3 * * ? *)"
+  schedule_expression_timezone = "UTC"
+
+  target {
+    arn      = "arn:aws:lambda:us-east-1:603013471165:function:weather-pdf-downloader"
+    role_arn = aws_iam_role.scheduler_invoke_lambda_role.arn
+  }
 }
